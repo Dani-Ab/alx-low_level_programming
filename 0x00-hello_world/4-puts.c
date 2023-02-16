70129e84d86ab15c8d8a468d1f52554c992378f0
daniel abraham <dani7ab@gmail.com>
2023-02-16 14:25:43 -0800

    compiles a C file and creates an executable

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (sucsess)
+ */
+
+int main(void)
+{
+	puts("\"programing is like building a multilingual puzzle");
+	return (0);
+
+
+}
+
