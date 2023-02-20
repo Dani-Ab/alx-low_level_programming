58f3958c563866dc212f4db82022a95c72b4ca66
daniel abraham <dani7ab@gmail.com>
2023-02-20 14:21:52 -0800

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,26 +14,26 @@ int main(void)
 	{
 		for (x = '0'; x <= '9'; x++)
 		{
-			for (y = x + 1; y <= '9'; y++)
+			for (y = '0'; y <= '9'; y++)
 			{
-				for (z = y + 1; z <= '9'; z++)
+				for (z = '0'; z <= '9'; z++)
 				{
 					if ((u != y) && (x != z))
 					{
-						putchar(u);
-						putchar(x);
+					putchar(u);
+					putchar(x);
+					putchar(' ');
+					putchar(y);
+					putchar(z);
+					if ((u == '9' && x == '8') && (y == '9' && z == '9'))
+					{
+						continue;
+					}
+					else
+					{
+						putchar(',');
 						putchar(' ');
-						putchar(y);
-						putchar(z);
-						if ((u == '9' && x == '8') && (y == '9' && z == '9'))
-						{
-							continue;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					}
 					}
 				}
 			}
