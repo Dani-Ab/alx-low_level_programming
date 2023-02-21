2637ab41998b13ebe03e26d065a18760ae08c478
daniel abraham <dani7ab@gmail.com>
2023-02-21 13:40:02 -0800

    prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,13 @@ void print_alphabet_x10(void)
 	char alpha;
 	int n = 0;
 
-	if (n <= 10)
+	while (n <= 10)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
-		_putchar('\n');
-		alpha='a';
 		n++;
+		_putchar('\n');
 	}
 }
