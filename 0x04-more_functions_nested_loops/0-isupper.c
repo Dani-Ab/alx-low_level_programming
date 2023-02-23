59b659fc71e10315fa3a76e00ddc39cbadf00bab
daniel abraham <dani7ab@gmail.com>
2023-02-23 12:35:39 -0800

    function that checks for a digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - check the alphabet if its captial or note.
+ * _isdigit - check the inputes if its a number or note.
  * @c: input alphabet
  * Return: Always 0.
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (isupper(c))
+	if (isdigit(c))
 	{
 		return (1);
 	}
 	return (0);
 }
-
-
