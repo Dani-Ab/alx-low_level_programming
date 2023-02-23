06ab799095548ea2b725281d070488fa93c3dcae
daniel abraham <dani7ab@gmail.com>
2023-02-23 11:47:59 -0800

    -function that checks for uppercase character

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the alphabet if its captial or note.
+ * @c: inputalphabet
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+
