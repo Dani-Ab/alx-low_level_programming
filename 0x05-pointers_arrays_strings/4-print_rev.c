ed43bd590f613477ec29e7d6ad5f2e01e77b3ff9
daniel abraham <dani7ab@gmail.com>
2023-02-27 16:22:09 -0800

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse order
+ * @s: input string.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+}
