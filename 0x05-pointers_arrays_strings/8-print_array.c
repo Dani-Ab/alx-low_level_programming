009ae9ee83405bccffb4998d42b1de6086a8d92a
daniel abraham <dani7ab@gmail.com>
2023-02-28 13:01:39 -0800

    prints n elements of an arrayof integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array : print arrayelements
+ *
+ * @a : array pointer
+ * @n : array size
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
+	}
+	putchar('\n');
+}
