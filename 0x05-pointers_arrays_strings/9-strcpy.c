c036ae6f7738e5ee0570b509aa59cb039433fc9a
daniel abraham <dani7ab@gmail.com>
2023-02-28 13:37:44 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy - to copy string values to temp file
+ * @dest: buffer pointer
+ * @src: main pointer
+ * Return: src arrays on temp from dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+}
