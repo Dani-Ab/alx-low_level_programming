c975f6fe03f8e370c52feda61ac301ee4a60d6d1
Dani-Ab <dani7ab@gmail.com>
2023-03-01 13:27:56 -0800

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenates two strings with difined number
+ * @dest : string input1
+ * @src :string input2
+ * @n : number input for string2
+ *
+ * Return: copy sring
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
