03e6c357c826fcba8e97024c521c7db99d310706
Dani-Ab <dani7ab@gmail.com>
2023-03-03 05:45:19 -0800

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,17 +10,22 @@
 char *rot13(char *str)
 {
 	int i = 0;
+	/*long int cary;*/
 
 	while (str[i])
 	{
 		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 		{
 			if ((str[i] > 'm' && str[i] <= 'z') || (str[i] > 'M' && str[i] <= 'Z'))
+			{
 				str[i] -= 13;
+			}
 			else
+			{
 				str[i] += 13;
+			}
 		}
-		i++;
+	i++;
 	}
 	return (str);
 }
