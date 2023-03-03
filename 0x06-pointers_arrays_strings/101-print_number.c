6233bdf4595796b92549f106366979762114f4a1
Dani-Ab <dani7ab@gmail.com>
2023-03-03 14:08:24 -0800

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,6 +10,7 @@
 void print_number(int n)
 {
 	unsigned int i;
+
 	if (n < 10 && n >= 0)
 	{
 		_putchar('0' + n);
@@ -21,9 +22,9 @@ void print_number(int n)
 		_putchar('-');
 	}
 	else
-        {
-                i = n;
-        }	
+	{
+		i = n;
+	}
 	if (i >= 10)
 	{
 		print_number(i / 10);
