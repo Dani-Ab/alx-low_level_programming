d1ebced8096c599c21ec9d42fe762aa295a5de52
Dani-Ab <dani7ab@gmail.com>
2023-03-05 06:10:18 -0800

    prints n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,8 +12,8 @@ void print_times_table(int n)
 	int i = 0;
 	int m = 0;
 	int mult;
-	
-	while((0 <= n) || (n < 15)  )
+
+	while ((n <= 0) || (n < 15))
 	{
 		while (i <= n)
 		{
@@ -21,11 +21,13 @@ void print_times_table(int n)
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
+			_putchar(' ');
 			for (m = 1 ; m < n; m++)
 			{
 				mult = i * m;
 				_putchar('0' + mult);
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 			}
