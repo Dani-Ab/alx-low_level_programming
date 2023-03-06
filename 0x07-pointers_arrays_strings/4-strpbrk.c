eb9e0139db8fd6a1bda0b0cfd4421da3b943dc6d
Dani-Ab <dani7ab@gmail.com>
2023-03-06 14:33:39 -0800

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to check maching bytes
+ * @accept: string containing maching bytes
+ *
+ * Return: matching
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (&accept[i]);
+		}
+		s++;
+	}
+	return (NULL);
+}
