1abcfe82810a4a2d535e6eb292b9b3e8db41a783
Dani-Ab <dani7ab@gmail.com>
2023-03-08 13:36:37 -0800

    prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string in revers
+ * @s: input string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
