64b97d74968e44a56d3d217eba76a2a03ab393f7
Dani-Ab <dani7ab@gmail.com>
2023-03-09 13:14:48 -0800

    checke if a number is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,27 +8,25 @@
 
 int is_prime_number(int n)
 {
-	if (n > 1)
-		return (prime_cal(n, 2));
-	else if (n < 0)
+	if (n <= 0)
 		return (0);
-	return (0);
+	return (is_prime_cal(n, 2));
 }
 
 /**
- * prime_cal - assisit by Checkig number is prime
+ * is_prime_cal - assisit by Checkig number is prime
  * @n: input
  * @m: test input
  *
  * Return: 1  prime, 0 otherwise
  */
 
-int prime_cal(int n, int m)
+int is_prime_cal(int n, int m)
 {
 	if ((n % m == 0) && (m != (n / 2))
 		return (0);
 	else if (m >= (n / 2))
 		return (1);
 	else
-		return (prime_cal(n, m + 1));
+		return (is_prime_cal(n, m + 1));
 }
