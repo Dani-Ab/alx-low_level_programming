8a9a446da90d46fb16adb3685f739809a7d1ffcd
Dani-Ab <dani7ab@gmail.com>
2023-03-12 12:27:11 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 
 /**
  * main - print the program name
- * argc: argumet count number
- * argv: variable argumets passed
+ *
+ * @argc: argument count number
+ * @argv: variables passed aruments
  *
  * Return: 0 sucesses
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
