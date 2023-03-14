a805617d20c3e5a73c581747e82188da812760f4
Dani-Ab <dani7ab@gmail.com>
2023-03-14 15:39:53 -0700

    function that frees a 2 dimensional grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,39 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 #include <string.h>
 
 /**
- * free_grid - concatinate two strings
+ * free_grid - frees a 2 dimensional grid of alloc_grid
  *
- * @grid: string one
- * @height: string two to be concatinat on s1
- * Return: pointer to a copy string  or NULL if its not sucesses
+ * @grid: 2d array
+ * @height: height of 2d array
+ * Return: nothing
  */
 
 void free_grid(int **grid, int height)
 {
-        unsigned int i = 0, n = 0, size1 = 0, size2 = 0, sizec = 0;
-        char *ptr;
+	int i;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
-        size1 = strlen(s1);
-        size2 = strlen(s2);
-        sizec = size1 + size2 + 1;
-        ptr = malloc(sizec * sizeof(char));
-        if (ptr == NULL)
-                return (NULL);
-        while (s1[i] != '\0')
-        {
-                ptr[i] = s1[i];
-                i++;
-        }
-        while (s2[n] != '\0')
-        {
-                ptr[i] = s2[n];
-                i++;
-                n++;
-        }
-        ptr[i] = '\0';
-        return (ptr);
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
