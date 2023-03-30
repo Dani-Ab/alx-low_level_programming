a9cd276bd4c3f29b3fabc0fe96dafa041b50f7b5
Dani-Ab <dani7ab@gmail.com>
2023-03-30 16:12:14 -0700

    retun number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,16 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t s = 0;
+	size_t i = 0;
 
 	while (h)
 	{
-		/*if (!h->str)*/
-			/* printf("[0] (nil)\n");*/
-		/* else*/
-			/* printf("[%u] %s\n", h->len, h->str);*/
 		h = h->next;
-		s++;
+		i++;
 	}
-	return (s);
+	return (i);
 }
