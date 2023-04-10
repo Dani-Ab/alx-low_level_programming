f04f572597f6e74801080e09f8ef9c542e0fbd27
Dani-Ab <dani7ab@gmail.com>
2023-04-10 16:40:49 -0700

    create a file and write a string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,13 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 	text_len = i;
-	file_cr = open("filename", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_cr = open("filename", O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_cr == -1)
 		return (-1);
 	if (text_content)
 		file_wr = write(file_cr, text_content, text_len);
+	if (file_wr == -1)
+		return (-1);
 	close(file_cr);
 	return (1);
 }
