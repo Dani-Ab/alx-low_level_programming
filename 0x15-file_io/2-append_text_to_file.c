26e4cc521237afb156b84f7bb17d667cdc0f7b1b
Dani-Ab <dani7ab@gmail.com>
2023-04-10 16:51:23 -0700

    create a file and write a string

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,4 @@
+int append_text_to_file(const char *filename, char *text_content);
 #include "main.h"
 /**
  * create_file - creat file wiht given string to be writen
@@ -7,13 +8,17 @@
  * Return: 1 for success -1 on failure
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_cr, file_wr, text_len;
-
+	int file_cr, file_wr, text_len, i = 0;
+	
 	if (filename == NULL)
 		return (-1);
-	text_len = strlen(text_content);
+	while (text_content[i])
+	{
+		i++;
+	}
+	text_len = i;
 	file_cr = open("filename", O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_cr == -1)
 		return (-1);
@@ -23,4 +28,4 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	close(file_cr);
 	return (1);
-}
+
