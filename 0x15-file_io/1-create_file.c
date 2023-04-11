87f2252a9002f8ddf024f87c2c12c6b176bb143a
Dani-Ab <dani7ab@gmail.com>
2023-04-10 17:17:17 -0700

    create a file and write a string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,11 @@ int create_file(const char *filename, char *text_content)
 	if (file_cr == -1)
 		return (-1);
 	if (text_content)
+	{
 		file_wr = write(file_cr, text_content, text_len);
-	if (file_wr == -1)
-		return (-1);
+		if (file_wr == -1)
+			return (-1);
+	}
 	close(file_cr);
 	return (1);
 }
